b5b3af49fac560fb1b91d010a0518012ec6f1183
Douglas Pacor <douglas@epicquestti.com.br>
2023-12-28 18:24:16 -0300

    doing

--- main/Game/Classes/GameObject.ts
+++ main/Game/Classes/GameObject.ts
@@ -1,33 +1,48 @@
-import { Vector2 } from "../../../renderer/Game/Classes/Vector2";
-
 export class GameObject {
-  private position: Vector2;
   private children: GameObject[];
   private parent: GameObject;
   private hasReadyBeenCalled: boolean;
 
-  constructor(position?: Vector2) {
-    this.position = position ?? new Vector2(0, 0);
+  constructor() {
     this.children = [];
     this.parent = null;
     this.hasReadyBeenCalled = false;
   }
 
   stepEntry(delta, root) {
-    // Call updates on all children first
     this.children.forEach((child) => child.stepEntry(delta, root));
 
-    // Call ready on the first frame
     if (!this.hasReadyBeenCalled) {
       this.hasReadyBeenCalled = true;
       this.ready();
     }
 
-    // Call any implemented Step code
     this.step(delta);
   }
 
   private ready = () => {};
 
-  private step = (_delta) => {};
+  private step = (deltaTime: number) => {};
+
+  destroy() {
+    this.children.forEach((child) => {
+      child.destroy();
+    });
+
+    this.parent.removeChild(this);
+  }
+
+  addChild(gameObject: GameObject) {
+    gameObject.parent = this;
+
+    this.children.push(gameObject);
+  }
+
+  removeChild(gameObject: GameObject) {
+    // events.unsubscribe(gameObject);
+
+    this.children = this.children.filter((g) => {
+      return gameObject !== g;
+    });
+  }
 }
