9669846ca16902fe222ef44570a60b28109dd2e0
Douglas Pacor <douglas@epicquestti.com.br>
2024-01-10 01:19:42 -0300

    doing

--- renderer/Game/objects/characterFrame/CharacterFrame.ts
+++ renderer/Game/objects/characterFrame/CharacterFrame.ts
@@ -3,9 +3,21 @@ import { GameObject } from "../../classes/GameObject";
 export class CharacterFrame extends GameObject {
   private life: { total: number; actual: number };
   private mana: { total: number; actual: number };
+  private percentLife: number;
+  private percentMana: number;
 
-  constructor(name?: string) {
-    super(null, name);
+  constructor() {
+    super(null);
+
+    this.life = { total: 100, actual: 49 };
+    this.mana = { total: 34, actual: 12 };
+    this.percentLife = (this.life.total / this.life.actual) * 100;
+    this.percentLife = (this.mana.total / this.mana.actual) * 100;
+  }
+
+  step(_delta: number): void {
+    this.percentLife = (this.life.total / this.life.actual) * 100;
+    this.percentLife = (this.mana.total / this.mana.actual) * 100;
   }
 
   draw(ctx: CanvasRenderingContext2D, x: number, y: number): void {
